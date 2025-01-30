48ac93ebf47b774765d95a1dca018936e04215a4
Bruno Henrique <brunobhhf@gmail.com>
2025-01-29 21:38:50 -0300

    Utilizando page para listar medicos

--- Java Desenvolvimento Back-End/Java/Spring Boot 3 desenvolva uma API Rest em java/api/src/main/java/mad/voll/api/controller/MedicoController.java
+++ Java Desenvolvimento Back-End/Java/Spring Boot 3 desenvolva uma API Rest em java/api/src/main/java/mad/voll/api/controller/MedicoController.java
@@ -3,10 +3,16 @@ package mad.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import mad.voll.api.medico.DadosCadastroMedico;
+import mad.voll.api.medico.DadosListagemMedico;
 import mad.voll.api.medico.Medico;
 import mad.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,5 +25,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
