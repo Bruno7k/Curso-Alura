d9d28a14d8f6ac6990f7a60af2caa0ce0c39de37
Bruno Henrique <brunobhhf@gmail.com>
2025-02-05 11:30:57 -0300

    adicionando tratamento de padrao de negocios

--- Java Desenvolvimento Back-End/Java/Spring Boot 3 documente, teste e prepare uma API para o deploy/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ Java Desenvolvimento Back-End/Java/Spring Boot 3 documente, teste e prepare uma API para o deploy/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -19,22 +22,29 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public void agendar(DadosAgendamentoConsulta dados) {
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
+    public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados) {
         if(!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException("O paciente informado não existe!");
         }
-        if(!medicoRepository.existsById(dados.idMedico())) {
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
             throw new ValidacaoException("O médico informado não existe!");
         }
-
+        validadores.forEach(v -> v.validar(dados));
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        if(medico == null){
+            throw new ValidacaoException("Não existe médico disponível na data solicitada!");
+        }
         var consulta = new Consulta(null, medico, paciente, dados.data());
         repository.save(consulta);
+        return new DadosDetalhamentoConsulta(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
-        if(dados.idPaciente() != null) {
+        if(dados.idMedico() != null) {
             return medicoRepository.getReferenceById(dados.idMedico());
         }
         if(dados.especialidade() == null) {
---
+++ Java Desenvolvimento Back-End/Java/Spring Boot 3 documente, teste e prepare uma API para o deploy/api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidarPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidarPacienteSemOutraConsultaNoDia implements ValidadorAgendamentoDeConsulta {
+    @Autowired
+    private ConsultaRepository consulta;
+    public void validar(DadosAgendamentoConsulta dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = consulta.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+        if(pacientePossuiOutraConsultaNoDia) {
+            throw new ValidacaoException("O paciente já possui uma consulta agendada no dia!");
+        }
+    }
+}
