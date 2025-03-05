de260fad8ca730fc9c73e0a4bf6111bf2a3072dd
Bruno Henrique <brunobhhf@gmail.com>
2025-03-05 18:22:59 -0300

    terminando o curso microsservicos na pratica

---
+++ Java Desenvolvimento Back-End/Java/Java gestão memória/gestao-memoria-vollmed-projeto_inicial/src/main/java/med/voll/api/testes/memoria/TestePC.java
@@ -0,0 +1,12 @@
+package med.voll.api.testes.memoria;
+
+public class TestePC {
+    public static void main(String[] args) {
+        System.out.println(soma(1,2));
+        System.out.println(soma(3,4));
+    }
+
+    public  static int soma(int valor1, int valor2){
+        return valor1+valor2;
+    }
+}
