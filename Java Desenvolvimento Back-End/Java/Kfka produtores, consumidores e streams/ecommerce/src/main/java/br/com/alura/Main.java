53349c6ece3b5abf7a9add6946594a0035ceacf2
Bruno Henrique <brunobhhf@gmail.com>
2025-04-08 14:24:32 -0300

    terminando a parte de kafka

--- Java Desenvolvimento Back-End/Java/Kfka produtores, consumidores e streams/ecommerce/src/main/java/br/com/alura/Main.java
+++ Java Desenvolvimento Back-End/Java/Kfka produtores, consumidores e streams/ecommerce/src/main/java/br/com/alura/Main.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,18 +17,25 @@ public class Main {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "123123,123122,122123123";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) ->{
+        var email = "Welcome! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, getCallback()).get();
+        producer.send(emailRecord, getCallback()).get();
+    }
+
+    private static Callback getCallback() {
+        return (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("Sucesso " + data.topic() + ":::" + data.partition() + ":::" + data.offset() + ":::" + data.timestamp());
-        }).get();
+        };
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9093");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
